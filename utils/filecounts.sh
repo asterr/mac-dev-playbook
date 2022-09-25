b8e6bc9dde704a7c543f0bcdad0603dfd63af329
Aaron Sterr <asterr@pobox.com>
2022-09-25 14:38:24 +0900

    exclude top level directory

--- utils/filecounts.sh
+++ utils/filecounts.sh
@@ -2,4 +2,4 @@
 
 basedir=$(dirname $0)
 
-find "${1}" -type d -maxdepth 1 ! -name . -exec ${basedir}/count.sh {} \; | sort -n
+find "${1}" -type d -maxdepth 1 ! -path "${1}" -exec ${basedir}/count.sh {} \; | sort -n
